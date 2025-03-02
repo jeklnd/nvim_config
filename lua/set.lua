9cf5e645d93f73a7672672e3574651d44ed9d9b9
Jesse Krensel <jdkrensel@gmail.com>
2025-03-02 01:36:13 -0500

    update sets

--- lua/set.lua
+++ lua/set.lua
@@ -15,7 +15,9 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.textwidth = 50
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -26,7 +28,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "72"
 
 vim.g.mapleader = " "
 
