e6feaeed43230b883fdfae1efc24826969baeb89
Jesse Krensel <jdkrensel@gmail.com>
2025-03-01 23:56:39 -0500

    add sets

--- lua/set.lua
+++ lua/set.lua
@@ -22,3 +22,10 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
